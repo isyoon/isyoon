c9aeb299598ddd56d80db38bfb83cca25cf12618
isyoon <isyoonn@gmail.com>
2011-12-04 00:47:03 -0800

    skills chart added.

--- js/resume.js
+++ js/resume.js
@@ -3,6 +3,7 @@ var Resume = function(){
       that = {
     init : function(options){
       maps = options.maps;
+      google.setOnLoadCallback(that.drawSkills);
       return that;
     },
     printMaps : function(){
@@ -34,6 +35,26 @@ var Resume = function(){
       }
       return that;
     },
+   
+    drawSkills : function(){
+      var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Year');
+        data.addColumn('number', 'Skill');
+        data.addRows(4);
+        data.setValue(0, 0, 'JS');
+        data.setValue(0, 1, 8);
+        data.setValue(2, 0, 'HTML/CSS');
+        data.setValue(1, 1, 8);
+        data.setValue(1, 0, 'JAVA');
+        data.setValue(2, 1, 5);
+        data.setValue(3, 0, 'SQL');
+        data.setValue(3, 1, 5);
+
+        var chart = new google.visualization.BarChart(document.getElementById('skillsChart'));
+        chart.draw(data, {width: 320, height: 240, title: 'Yoon\'s skills',
+                          vAxis: {title: 'Skills'}
+                         });
+    }
   }
   return that;
 };
